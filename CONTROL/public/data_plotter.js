da9599c48b62a06c9f5b911793690dfdf5ceb710
Nik Martelaro <nmartelaro@gmail.com>
2020-04-27 10:46:05 -0400

    creating auto data viewer

---
+++ CONTROL/public/data_plotter.js
@@ -0,0 +1,26 @@
+function setup() {
+  let width = 500;
+  let height = 430;
+  var canvas = createCanvas(width, height);
+  canvas.parent('data');
+}
+
+function draw() {
+  background(220);
+  drawAxes();
+  
+}
+
+function drawAxes() {
+  let length = width/2 - 20;
+
+  // Speed
+  line(20, 100, width-20, 100);
+  // Accel
+  line(20, height-150, length, height-150);
+  line((height-150)/2-10, height-150-75, (height-150)/2-10, (height-150)/2-10+length);
+  
+  // Gyro
+  line(250, height-150, width-20, height-150);
+  line(width-20-(length/2), height-150-75, width-20-(length/2), (height-150)/2-10+length);
+}
