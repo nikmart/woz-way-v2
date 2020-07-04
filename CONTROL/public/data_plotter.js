0c474634aa36093a93800551cb2dd9198aee28d8
Nik Martelaro <nmartelaro@gmail.com>
2020-07-04 16:50:14 -0400

    scrolling live data

--- CONTROL/public/data_plotter.js
+++ CONTROL/public/data_plotter.js
@@ -3,15 +3,18 @@ var socket = io();
 var data = [
 ]
 
+var minimumTime = new Date();
+
 var vlSpec = {
   $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
   width: "container",
   height: "container",
+  "background": "WhiteSmoke",
   data: { name: 'table' },
   mark: 'line',
   encoding: {
-    x: { timeunit: "utcminutesseconds", field: 'time', type: 'temporal'},
-    y: { field: 'value', type: 'quantitative' },
+    x: { timeunit: "minutes", field: 'time', type: 'temporal'},
+    y: { field: 'value', type: 'quantitative', "scale": {"domain": [-15, 15]} },
     "color": { "field": "sensor", "type": "nominal" }
   }
 };
@@ -19,12 +22,23 @@ var vlSpec = {
 vegaEmbed('#vis', vlSpec, { defaultStyle: true })
   .then(function (result) {
     const view = result.view;
-    
     // Update accel coordinates
     socket.on('data', function (msg) {
       // Use the Vega view api to insert data
       //console.log(msg);
       view.insert("table", JSON.parse(msg)).run();
     });
+    window.setInterval(function () {
+      minimumTime = Date.parse(new Date()) - 30000;
+      //console.log(minimumTime);
+      var changeSet = vega
+        .changeset()
+        .remove(function (t) {
+          // console.log(Date.parse(t.time));
+          // console.log(minimumTime);
+          return Date.parse(t.time) < minimumTime;
+        });
+      result.view.change('table', changeSet).run();
+    }, 300);
   })
   .catch(console.warn);
